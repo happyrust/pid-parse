84d177a3b9affca5b7444d37fb030604eca701bf
happyrust <96458753+happyrust@users.noreply.github.com>
2026-04-06 00:52:04 +0800

    Add library module exports

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod api;
+pub mod cfb;
+pub mod error;
+pub mod inspect;
+pub mod model;
+pub mod parsers;
+pub mod streams;
+
+pub use api::{ParseOptions, PidParser};
+pub use error::PidError;
+pub use model::*;
