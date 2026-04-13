d0f3f1a90fc7efdd944c3f36faf74f46118767bd
happyrust <96458753+happyrust@users.noreply.github.com>
2026-04-13 14:41:07 +0800

    Add tagged text stream parser

---
+++ src/streams/tagged_text.rs
@@ -0,0 +1,24 @@
+use crate::api::ParseOptions;
+use crate::error::PidError;
+use crate::model::PidDocument;
+use std::io::Read;
+
+pub fn parse_tagged_text_streams<R: Read + std::io::Seek>(
+    cfb: &mut ::cfb::CompoundFile<R>,
+    doc: &mut PidDocument,
+    _options: &ParseOptions,
+) -> Result<(), PidError> {
+    if let Ok(mut s) = cfb.open_stream("/TaggedTxtData/Drawing") {
+        let mut xml = String::new();
+        s.read_to_string(&mut xml)?;
+        doc.drawing_meta = Some(crate::parsers::drawing_xml::parse_drawing_xml(&xml)?);
+    }
+
+    if let Ok(mut s) = cfb.open_stream("/TaggedTxtData/General") {
+        let mut xml = String::new();
+        s.read_to_string(&mut xml)?;
+        doc.general_meta = Some(crate::parsers::general_xml::parse_general_xml(&xml)?);
+    }
+
+    Ok(())
+}
